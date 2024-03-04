284cf2f96013f6caa7018705273704c9ed095335
tianhr <your.email@example.com>
2024-03-04 14:20:18 +0800

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface ICustomersProps {}
+
+export default function Customers(props: ICustomersProps) {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface IInvoicesProps {}
+
+export default function Invoices(props: IInvoicesProps) {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  );
+}
